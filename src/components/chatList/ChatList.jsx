e371ac4f6afe973ef081a1561feac2f6c2bfdc67
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-03-05 18:14:56 +0530

    home layout and dashboard layout finished

---
+++ src/components/chatList/ChatList.jsx
@@ -0,0 +1,58 @@
+import { Link } from 'react-router-dom'
+import './chatList.css'
+
+const ChatList = () => {
+  return (
+    <div className='chatList'>
+        <span className='title'>DASHBOARD</span>
+        <Link to='/dashboard'>Create a New Chat</Link>
+        <hr />
+        <span className='title'>RECENT CHATS</span>
+        <div className="list">
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+            <Link to='/'>New Chat</Link>
+        </div>
+        <hr />
+        <div className="footer">
+            <span>Created By Piyush K. Bhattacharyya</span>
+            <span>Home Assignment: Sarvam AI</span>
+        </div>
+    </div>
+  )
+}
+
+export default ChatList
